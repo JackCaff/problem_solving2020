2653ca4fe43f0a0d06ea9ab5f6028736b0d4786a
JackCaff <JackCaff@users.noreply.github.com>
2020-03-01 21:10:12 +0000

    Upload Weekly Task 5

--- WT05/WT05.py
+++ WT05/WT05.py
@@ -1,24 +1,17 @@
-# Allows the user know what day of the week it is
-# trial program to understand logic 
 #Jack Caffrey 
+# Allows the user know what day of the week it is
+ 
+import datetime # Imports current time and date from library 
 
-import datetime
+now = datetime.datetime.now()  
+# print("now", now) display the exact time the progam is run 
 
-now = datetime.datetime.now()
-
-print ("now", now)
-
-day = now.day
-
-print ("Day is", day)
+day = now.day 
+# print("Date is", day) if required can display the exact date the progam is run
 
 weekday = now.weekday()
 
-
-if weekday == [0,1,2,3,4]:
-
-    print (" Yes, unfortunately today is a weekday")
-
+if weekday == (0,1,2,3,4):
+     print (" Yes, unfortunately today is a weekday")
 else: 
-
     print ("It is the weekend, yay!")
