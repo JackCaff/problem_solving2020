91928863096bee8ce5bf42adfbaccf84b047975d
JackCaff <JackCaff@users.noreply.github.com>
2020-03-05 20:31:27 +0000

    Added weekly task 7

---
+++ WT07/WT07.py
@@ -0,0 +1,15 @@
+# Jack Caffrey
+
+f = open('moby10b.txt', 'r')  #defines the file to read in the arguments
+contents = f.read() #reads the content of the file 
+count = 0  #Initialize the counter
+for i in contents:
+    if i == 'e':
+        count = count + 1 #counter increments 1 every time the file reads lower case e
+
+print (count) # prints the values of 3 contained in the txt file
+
+f.close() #closes read file
+
+
+ 
