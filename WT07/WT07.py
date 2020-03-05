33180a2342ba9b4d828063424cf696676f2a0680
JackCaff <JackCaff@users.noreply.github.com>
2020-03-05 20:32:46 +0000

    updated comments in code

--- WT07/WT07.py
+++ WT07/WT07.py
@@ -7,7 +7,7 @@ for i in contents:
     if i == 'e':
         count = count + 1 #counter increments 1 every time the file reads lower case e
 
-print (count) # prints the values of 3 contained in the txt file
+print (count) # prints the values of e's contained in the txt file
 
 f.close() #closes read file
 
